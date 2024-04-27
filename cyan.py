2c74cdb0b3b6aba516b6e543d1b7c18248d5cc0c
zx <zxcvbn@hrzn.email>
2024-04-26 20:04:27 -0400

    chore: remove unnecessary typing

--- cyan.py
+++ cyan.py
@@ -1,27 +1,32 @@
+#!/usr/bin/python3
 # cyan, aka pyzule-rw; by zx, 2024
 
 import os
 import sys
 import argparse
 
-parser: argparse.ArgumentParser = argparse.ArgumentParser(
+parser = argparse.ArgumentParser(
     description="cyan, an azule \"clone\" for modifying iOS apps")
 parser.add_argument("-o", metavar="output",
                     help="if unspecified, overwrites input")
 parser.add_argument("-f", metavar="file", nargs="+",
                     help="a tweak to inject/item to be added to the bundle")
-parser.add_argument("input", help="the app to be modified (.app/.ipa)")
-args: argparse.Namespace = parser.parse_args()
 
-INPUT: str = os.path.realpath(args.input)
-WORKING_DIR: str = os.getcwd()
-PZ_DIR: str = os.path.expanduser("~/.config/cyan")
-changed: bool = False
+parser.add_argument("--overwrite", action="store_true",
+                    help="skip overwrite confirmation")
+
+parser.add_argument("input", help="the app to be modified (.app/.ipa)")
+args = parser.parse_args()
+
+INPUT = os.path.realpath(args.input)
+WORKING_DIR = os.getcwd()
+PZ_DIR = os.path.expanduser("~/.config/cyan")
+changed = False
 
 if not (INPUT.endswith(".ipa") or INPUT.endswith(".app")):
     sys.exit("[!] input must be a .ipa/.app")
 elif not os.path.exists(INPUT):
     sys.exit("[!] input does not exist")
 elif not args.o and (input("[<] override your input file? [y/N] ").strip()
-                    .lower() not in ("y", "yes")):
+                     .lower() not in ("y", "yes")):
     sys.exit("[!] not overriding input, missing -o")
